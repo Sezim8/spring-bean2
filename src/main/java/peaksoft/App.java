e42f343fb4874427230c03c060c0b74cc480a182
sezim <sezimmarlisova@gmail.com>
2021-10-14 12:09:46 +0600

    hw

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -2,6 +2,7 @@ package peaksoft;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 public class App {
     public static void main(String[] args) {
@@ -10,5 +11,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean2.getMessage());
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println(bean==bean2);
+
+        System.out.println(cat==cat2);
+
+
+
+
     }
 }
